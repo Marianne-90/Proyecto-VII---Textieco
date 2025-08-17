4f6d652f85e73760f864cfa28c5283acc048e10d
Marianne <marianne.garridom@gmail.com>
2025-08-17 17:44:42 +0200

    Admin Edit Brand

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use App\Models\Brand;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
 use Intervention\Image\Laravel\Facades\Image;
 
 class AdminController extends Controller
@@ -44,6 +45,40 @@ class AdminController extends Controller
         return redirect()->route('admin.brands')->with('status', 'Brand has been added succesfully');
     }
 
+    public function brand_edit($id){
+        $brand = Brand::find($id);
+        return view('admin.brand-edit', compact('brand'));
+    }
+
+    public function brand_update(Request $request){
+                $request->validate(
+            [
+                'name'=> 'required',
+                'slug' => 'required|unique:brands,slug,'.$request->id,
+                'image' => 'mimes:png,jpg,jpeg|max:2048'
+            ]
+            );
+        $brand = Brand::find($request->id);
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        if($request->hasFile('image')){
+
+            if(File::exists(public_path('uploads/brands').'/'.$brand->image)){
+                File::delete(public_path('uploads/brands').'/'.$brand->image);
+            }
+
+        $image = $request-> file('image');
+        $file_extention = $request->file('image')->extension();
+        $file_name = Carbon::now()->timestamp.".". $file_extention;
+        $this->GenerateBrandThumbailsImage($image, $file_name);
+        $brand->image = $file_name;
+
+        }
+
+        $brand->save();
+        return redirect()->route('admin.brands')->with('status', 'Brand has been udated succesfully');
+    }
+
     public function GenerateBrandThumbailsImage($image, $imageName) {
         $destinationPath = public_path('uploads/brands');
         $img = Image::read($image->path());
@@ -51,5 +86,6 @@ class AdminController extends Controller
         $img->resize(124,124, function($constraint){
             $constraint->aspectRatio();
         })->save($destinationPath.'/'.$imageName);
-    }
+
+}
 }
