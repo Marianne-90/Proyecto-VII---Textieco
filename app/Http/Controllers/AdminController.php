ba1ec31f96440f704bdd524ee5794169c51a699f
Marianne <marianne.garridom@gmail.com>
2025-08-17 21:51:00 +0200

    Admin Delete Category

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -180,5 +180,18 @@ public function category_add(){
         return redirect()->route('admin.categories')->with('status', 'Category has been udated succesfully');
     }
 
+    public function category_delete($id){
+    $category = Category::find($id);
+
+    if(File::exists(public_path('uploads/categories').'/'.$category->image)){
+        File::delete(public_path('uploads/categories').'/'.$category->image);
+    }
+
+    $category->delete();
+    return redirect()->route('admin.categories')->with('status', 'Category has been deleted succesfully');
+
+
+}
+
 
 }
