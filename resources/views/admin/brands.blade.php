2ffea441ca113b8d02524ef4aa56b348eb86bc68
Marianne <marianne.garridom@gmail.com>
2025-08-17 19:05:02 +0200

    Admin Delete Brand

--- resources/views/admin/brands.blade.php
+++ resources/views/admin/brands.blade.php
@@ -38,7 +38,7 @@
                 <div class="wg-table table-all-user">
                     <div class="table-responsive">
                         @if(Session::has('status'))
-                            <p class="table table-striped table-bordered">{{ Session::get('status') }}</p>
+                            <p class="alert alert-success text-center">{{ Session::get('status') }}</p>
                         @endif
                         <table class="table table-striped table-bordered">
                             <thead>
@@ -68,12 +68,16 @@
                                         <td><a href="#" target="_blank">0</a></td>
                                         <td>
                                             <div class="list-icon-function">
-                                                <a href="{{ route('admin.brand.edit', ['id'=>$brand->id])}}">
+                                                <a href="{{ route('admin.brand.edit', ['id' => $brand->id])}}">
                                                     <div class="item edit">
                                                         <i class="icon-edit-3"></i>
                                                     </div>
                                                 </a>
-                                                <form action="#" method="POST">
+                                                <form action="{{ route('admin.brand.delete', ['id' => $brand->id])}}"
+                                                    method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+
                                                     <div class="item text-danger delete">
                                                         <i class="icon-trash-2"></i>
                                                     </div>
@@ -94,3 +98,29 @@
         </div>
     </div>
 @endsection
+
+@push('scripts')
+    <script>
+        $(function () {
+            $('.delete').on("click", function (e) {
+                e.preventDefault();
+                var form = $(this).closest('form');
+                swal({
+                    title: "Are you sure?",
+                    text: "You want to delete this record?",
+                    type: "warning",
+                    buttons: ["NO", "YES"],
+                    confirmButtonColor: '#dc3545'
+                }).then(function (result) {
+                    if (result) {
+                        // console.log('➡️ Ruta a la que se enviará el formulario:', form.attr('action'));
+                        // console.log('➡️ Método HTML:', form.attr('method'));
+
+                        form.submit();
+                    }
+                });
+            });
+        });
+
+    </script>
+@endpush
