bbff6f849eee0bb893ad14391d56c13abcfaa899
Marianne <marianne.garridom@gmail.com>
2025-08-19 13:43:38 +0200

    Admin Edit Product

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -288,7 +288,7 @@ class AdminController extends Controller
         $product->save();
         return redirect()->route('admin.products')->with('status', 'Product has been added successfully');
     }
-//Guardar imágenes progucto
+    //Guardar imágenes progucto
     function GenerateProductThumbailsImage($image, $imageName): void
     {
         $destinationPathThumbnails = public_path('uploads/products/thumbnails');
@@ -306,4 +306,97 @@ class AdminController extends Controller
         })->save($destinationPathThumbnails . '/' . $imageName);
     }
 
+    public function product_edit($id)
+    {
+        $product = Product::find($id);
+        $categories = Category::select('id', 'name')->orderBy('name')->get();
+        $brands = Brand::select('id', 'name')->orderBy('name')->get();
+        return view('admin.product-edit', compact('product', 'categories', 'brands'));
+    }
+
+    public function product_update(Request $request)
+    {
+        $request->validate(
+            [
+                'name' => 'required',
+                'slug' => 'required|unique:products,slug,' . $request->id,
+                'short_description' => 'required',
+                'description' => 'required',
+                'regular_price' => 'required',
+                'sale_price' => 'required',
+                'SKU' => 'required',
+                'stock_status' => 'required',
+                'featured' => 'required',
+                'quantity' => 'required',
+                'image' => 'mimes:png,jpg,jpeg|max:2048',
+                'category_id' => 'required',
+                'brand_id' => 'required',
+            ]
+        );
+        $product = Product::find($request->id);
+
+        $product->name = $request->name;
+        $product->slug = Str::slug($request->name);
+        $product->short_description = $request->short_description;
+        $product->description = $request->description;
+        $product->regular_price = $request->regular_price;
+        $product->sale_price = $request->sale_price;
+        $product->SKU = $request->SKU;
+        $product->stock_status = $request->stock_status;
+        $product->featured = $request->featured;
+        $product->quantity = $request->quantity;
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+
+                $current_timestamp = Carbon::now()->timestamp;
+
+        if ($request->hasFile('image')) {
+
+            if(File::exists(public_path('uploads/products') . '/' . $product->image)) {
+                File::delete(public_path('uploads/products') . '/' . $product->image);
+            }
+            if(File::exists(public_path('uploads/products/thumbnails') . '/' . $product->image)) {
+                File::delete(public_path('uploads/products/thumbnails') . '/' . $product->image);
+            }
+
+            $image = $request->file('image');
+            $imageName = $current_timestamp . '.' . $image->extension();
+            $this->GenerateProductThumbailsImage($image, $imageName);
+            $product->image = $imageName;
+        }
+
+        $gallery_arr = array();
+        $gallery_images = "";
+        $counter = 1;
+
+        if ($request->hasFile('images')) {
+
+            foreach (explode(',', $product->images) as $ofile) {
+                if(File::exists(public_path('uploads/products') . '/' . $ofile)) {
+                    File::delete(public_path('uploads/products') . '/' . $ofile);
+                }
+                if(File::exists(public_path('uploads/products/thumbnails') . '/' . $ofile)) {
+                    File::delete(public_path('uploads/products/thumbnails') . '/' . $ofile);
+                }
+            }
+
+            $allowedfileExtension = ['png', 'jpg', 'jpeg'];
+            $files = $request->file('images');
+            foreach ($files as $file) {
+                $gextension = $file->getClientOriginalExtension();
+                $gcheck = in_array($gextension, $allowedfileExtension);
+                if ($gcheck) {
+                    $gfileName = $current_timestamp . '-' . $counter . '.' . $gextension;
+                    $this->GenerateProductThumbailsImage($file, $gfileName);
+                    array_push($gallery_arr, $gfileName);
+                    $counter++;
+                }
+            }
+            $gallery_images = implode(',', $gallery_arr);
+            $product->images = $gallery_images;
+        }
+        $product->save();
+        return redirect()->route('admin.products')->with('status', 'Product has been updated successfully.');
+
+    }
 }
