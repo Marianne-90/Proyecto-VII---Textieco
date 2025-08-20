68b9d78957550ad4d30606c3e2467c6cabc82f01
Marianne <marianne.garridom@gmail.com>
2025-08-20 12:10:07 +0200

    Product Sorting and Products Per Page

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -9,18 +9,47 @@ use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    public function index()  {
-        $products = Product::orderBy('created_at', 'desc')->paginate(12);
+    public function index(Request $request)
+    {
+        $size = $request->query('size', 12); // Default to 12 if 'size' is not provided
+        $o_column = "";
+        $o_order = "";
+        $order = $request->query('order', -1);
+
+        switch ($order) {
+            case 1:
+                $o_column = 'created_at';
+                $o_order = 'asc';
+                break;
+            case 2:
+                $o_column = 'created_at';
+                $o_order = 'desc';
+                break;
+            case 3:
+                $o_column = 'regular_price';
+                $o_order = 'asc';
+                break;
+            case 4:
+                $o_column = 'regular_price';
+                $o_order = 'desc';
+                break;
+            default:
+                $o_column = 'id';
+                $o_order = 'desc';
+                break;
+        }
+
+        $products = Product::orderBy($o_column, $o_order)->paginate($size);
         $categories = Category::orderBy('id', 'DESC')->get();
         $brands = Brand::orderBy('id', 'DESC')->get();
-        return view('shop', compact('products', 'categories', 'brands'));
+        return view('shop', compact('products', 'categories', 'brands', 'size', 'order'));
     }
 
-public function product_details($product_slug)
-{
-    $product = Product::where('slug', $product_slug)->first();
-    $rproducts = Product::where('category_id', $product->category_id)->where('id', '!=', $product->id)->get()->take(4);
-    return view('details', compact('product', 'rproducts'));
-}
+    public function product_details($product_slug)
+    {
+        $product = Product::where('slug', $product_slug)->first();
+        $rproducts = Product::where('category_id', $product->category_id)->where('id', '!=', $product->id)->get()->take(4);
+        return view('details', compact('product', 'rproducts'));
+    }
 
 }
