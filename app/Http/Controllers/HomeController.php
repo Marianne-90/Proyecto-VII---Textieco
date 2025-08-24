19c41dbc9db1429c1bfc14b6e45b0c45f1970428
Marianne <marianne.garridom@gmail.com>
2025-08-24 20:07:19 +0200

    Show On Sale Products On Home Page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use App\Models\Slide;
 use Illuminate\Http\Request;
 
@@ -13,13 +14,19 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */ /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
+       * Show the application dashboard.
+       *
+       * @return \Illuminate\Contracts\Support\Renderable
+       */
     public function index()
     {
         $slides = Slide::where('status', 1)->get()->take(3);
-        return view('index', compact('slides'));
+        $sProducts = Product::whereNotNull('sale_price')
+            ->where('sale_price', '<>', 0)
+            ->whereColumn('sale_price', '<>', 'regular_price')
+            ->inRandomOrder()
+            ->get()
+            ->take(8);
+        return view('index', compact('slides', 'sProducts'));
     }
 }
