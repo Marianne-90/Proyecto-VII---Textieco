fcea95b01847c9a555455dc8b043340349af6fbe
Marianne <marianne.garridom@gmail.com>
2025-11-15 12:49:36 +0100

    FIX pay

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -201,27 +201,12 @@
                                 </table>
                             </div>
                             <div class="checkout__payment-methods">
-                                <div class="form-check">
-                                    <input class="form-check-input form-check-input_fill" type="radio" name="mode"
-                                        id="mode1" value="card">
-                                    <label class="form-check-label" for="mode1">
-                                        Tarjeta de Débito o Crédito
 
-                                    </label>
-                                </div>
-
-                                <div class="form-check">
-                                    <input class="form-check-input form-check-input_fill" type="radio" name="mode"
-                                        id="mode2" value="paypal">
-                                    <label class="form-check-label" for="mode2">
-                                        Paypal
-                                    </label>
-                                </div>
                                 <div class="form-check">
                                     <input class="form-check-input form-check-input_fill" type="radio" name="mode"
                                         id="mode3" value="cod">
                                     <label class="form-check-label" for="mode3">
-                                        Contra reembolso
+                                        Pagar en la entrega
                                     </label>
                                 </div>
                                 <div class="policy-text">
